b3501313f4890e15160c3679f46ae2868cb35281
sigvardbratlie <sigvard.bratlie@gmail.com>
2026-01-21 16:26:16 +0100

    wokring with testdata

--- data/src/data/utils.py
+++ data/src/data/utils.py
@@ -2,6 +2,31 @@
 import PyPDF2
 from pathlib import Path
 import re
+from email import policy
+from email.parser import BytesParser
+from pydantic import BaseModel
+from email.utils import parsedate_to_datetime
+from typing import Optional
+import logging
+from datetime import datetime, timezone
+import docx
+import openpyxl
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+class ParsedAttachment(BaseModel):
+    filename : str
+    content : bytes
+
+class ParsedEmail(BaseModel):
+    sender : str
+    receiver : str
+    subject : str
+    timestamp : Optional[datetime]
+    body : str
+    attachments : Optional[list[ParsedAttachment]] = []
 
 
 def mk_txt_from_pdf(filepath_pdf, filepath_txt):
@@ -17,28 +42,154 @@ def mk_txt_from_pdf(filepath_pdf, filepath_txt):
         text_file.write(text)
 
 
+def parse_email(file : str) -> ParsedEmail:
+    with open(file, "rb") as f:
+        msg = BytesParser(policy=policy.default).parse(f)
+    time = parsedate_to_datetime(msg.get("Date",None))
+    if time is None:
+        time = msg.get("Date",None)
+    try:
+        email = ParsedEmail(sender = msg.get("From"),
+                            receiver=msg.get("To"),
+                            subject=msg.get("Subject",None),
+                            timestamp=time,
+                            body=msg.get_body(preferencelist=("plain",)).get_content(),
+                            )
+
+        for part in msg.iter_attachments():
+            filename = part.get_filename()
+            content = part.get_payload(decode=True)
+            email.attachments.append(ParsedAttachment(filename=filename, content=content))
+
+        return email
+    except Exception as e:
+        logger.error(f"Error parsing email {file}: {e}")
+        return None
+
+def check_correct_format(file : Path):
+    startswith_date = re.compile(r"^\d{4}-\d{2}-\d{2}_")
+    startswith_year = re.compile(r"^\d{4}_")
+    if startswith_date.match(file.name):
+        logger.info(f"----ALREADY CORRECT FORMAT: {file.name}----")
+        return True
+    elif startswith_year.match(file.name):
+        logger.info(f"----ALREADY CORRECT FORMAT: {file.name}----")
+        return True
+    else:
+        return False
+    
 
 
+def rename_email_with_dates(file : Path,rename = False,):
+    correct_format = check_correct_format(file)
+    if correct_format:
+        return
+    
+    with open(file, "rb") as f:
+        msg = BytesParser(policy=policy.default).parse(f)
 
-def rename_files_with_dates(rename = False,directory_path="fabricated",file_extension="txt"):
-    for file in Path(directory_path).glob(f"*.{file_extension}"):
-        #print(file.name)
-        pattern_date = re.compile(r"\d{4}-\d{2}-\d{2}")
-        pattern_year = re.compile(r"\d{4}")
-        match_date = pattern_date.search(file.name)
-        match_year = pattern_year.search(file.name)
-        if match_date:
-            new_filename = match_date.group(0) + "_" + file.name
-            if rename:
-                file.rename(file.parent / new_filename)
-            else:
-                print(new_filename)
-        elif match_year:
-            new_filename = match_year.group(0) + "_" + file.name
-            if rename:
-                file.rename(file.parent / new_filename)
-            else:
-                print(new_filename)
+    date = parsedate_to_datetime(msg.get("Date", None))
+    if isinstance(date, datetime):
+        date = date.date()
+    if date is None:
+        logger.warning(f"----NO DATE FOUND: {file.name}----")
+        return
+    if rename:
+        Path(file).rename(file.parent / (str(date) + "_" + file.name))
+    else:
+        logger.info(str(date) + "_" + file.name)
+    
+
+def rename_file_with_date(file : Path, rename = False,):
+    correct_format = check_correct_format(file)
+    if correct_format:
+        return
+    
+    pattern_date = re.compile(r"\d{4}-\d{2}-\d{2}")
+    pattern_year = re.compile(r"\d{4}")
+    match_date = pattern_date.search(file.name)
+    match_year = pattern_year.search(file.name)
+    if match_date:
+        new_filename = match_date.group(0) + "_" + file.name
+        if rename:
+            file.rename(file.parent / new_filename)
         else:
-            pass
-            print(f"----NO DATE FOUND: {file.name}----")
+            logger.info(new_filename)
+    elif match_year:
+        new_filename = match_year.group(0) + "_" + file.name
+        if rename:
+            file.rename(file.parent / new_filename)
+        else:
+            logger.info(new_filename)
+    else:
+        logger.warning(f"----NO DATE FOUND: {file.name}----")
+
+
+def parse_pdf_date(s: str):
+    if not s or not s.startswith("D:"):
+        return None
+
+    s = s[2:]
+    dt = datetime(
+        int(s[0:4]), int(s[4:6]), int(s[6:8]),
+        int(s[8:10]), int(s[10:12]), int(s[12:14]),
+        tzinfo=timezone.utc
+    )
+    return dt
+
+
+def rename_pdf_with_date(file, rename=False, create_date : bool = True):
+    '''Rename PDF file with creation date from metadata.'''
+    correct_format = check_correct_format(file)
+    if correct_format:
+        return
+    with open(file, 'rb') as f:
+        txt = ""
+        reader = PyPDF2.PdfReader(f)
+        meta = reader.metadata
+        for page in reader.pages:
+            txt += page.extract_text()
+        
+        if (meta.get('/CreationDate') or meta.get('/ModDate')) and txt:
+            if create_date and meta.get('/CreationDate'):
+                date = parse_pdf_date(meta['/CreationDate'])
+            else:
+                date = parse_pdf_date(meta['/ModDate'])
+            if rename:
+                Path(file).rename(file.parent / (str(date) + "_" + file.name))
+            else:
+                logger.info(str(date) + "_" + file.name)
+        else:
+            if not meta:
+                logger.warning(f"----NO METADATA FOUND: {file.name}----")
+            if not txt:
+                logger.warning(f"----NO TEXT FOUND: {file.name}----")
+
+
+def rename_xlsx_with_date(file : Path, rename=False):
+    '''Rename XLSX file with creation date from metadata.'''
+    correct_format = check_correct_format(file)
+    if correct_format:
+        return
+    
+    wb = openpyxl.load_workbook(file, read_only=True)
+    props = wb.properties
+    props.created
+    if rename:
+        file.rename(file.parent / f"{props.created.date()}_{file.name}")
+    else:
+        logger.info(f"{props.created.date()}_{file.name}")
+
+def rename_docx_with_date(file : Path, rename=False):
+    correct_format = check_correct_format(file)
+    if correct_format:
+        return
+    doc = docx.Document(file)
+    date = doc.core_properties.created
+    if not date:
+        logger.warning(f"----NO DATE FOUND: {file.name}----")
+        return
+    if rename:
+        Path(file).rename(file.parent / (str(doc.core_properties.created.date()) + "_" + file.name))
+    else:
+        logger.info(str(doc.core_properties.created.date()) + "_" + file.name)
